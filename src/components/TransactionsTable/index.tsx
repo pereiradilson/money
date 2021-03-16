c54fb4fda2cb07a7af57175709106d739ef99fc3
Adilson Pereira <adilsonpereiradesenvolvimento@gmail.com>
2021-03-15 22:33:13 -0300

    First commit

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,38 @@
+import { Container } from './styles';
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$ 12.000,00</td>
+            <td>Venda</td>
+            <td>05/03/2021</td>
+          </tr>
+          <tr>
+            <td>Aluguel do apartamento</td>
+            <td className="withdraw">- R$ 700,00</td>
+            <td>Casa</td>
+            <td>10/03/2021</td>
+          </tr>
+          <tr>
+            <td>Energia</td>
+            <td className="withdraw">- R$ 200,00</td>
+            <td>Casa</td>
+            <td>10/03/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
